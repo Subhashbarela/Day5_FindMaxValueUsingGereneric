12e64994699e83f0ea912e94cb448ac18062d0bf
Subhash Barela <barelasubhash16@gmail.com>
2023-07-06 15:57:34 +0530

    [subhash] find max of three integer number

--- MaxValueUsingGeneric/Program.cs
+++ MaxValueUsingGeneric/Program.cs
@@ -10,7 +10,10 @@ namespace MaxValueUsingGeneric
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello world");
+            CheckMaxValue intMax = new CheckMaxValue();
+
+            int max = intMax.MaximunIntegerNumber(10, 25, 20);
+            Console.WriteLine("Maximum Value Is : " + max);
             Console.ReadLine();
         }
     }
