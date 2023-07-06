985559d82e998e87c6c2db4012e2a75b3413fffa
Subhash Barela <barelasubhash16@gmail.com>
2023-07-06 16:07:28 +0530

    [subhash] find max of flaot value

--- MaxValueUsingGeneric/Program.cs
+++ MaxValueUsingGeneric/Program.cs
@@ -10,10 +10,30 @@ namespace MaxValueUsingGeneric
     {
         static void Main(string[] args)
         {
-            CheckMaxValue intMax = new CheckMaxValue();
-
-            int max = intMax.MaximunIntegerNumber(10, 25, 20);
-            Console.WriteLine("Maximum Value Is : " + max);
+            CheckMaxValue max = new CheckMaxValue();
+            Console.WriteLine("1: max of integer value \n2: max of float value");
+            Console.WriteLine("Enter the choice...!");
+            int choice=int.Parse(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    {
+                        int intMax = max.MaximunIntegerNumber(10, 25, 20);
+                        Console.WriteLine("Maximum int Value Is : " + intMax);
+                        break;
+                    }
+                case 2:
+                    {
+                        float floatMax = max.CheckMaximumFloatNumber(10.3f, 20.5f, 2.4f);
+                        Console.WriteLine("Maximum flaot Value Is : " + floatMax);
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Please insert valid option");
+                        break;
+                    }
+            }
             Console.ReadLine();
         }
     }
